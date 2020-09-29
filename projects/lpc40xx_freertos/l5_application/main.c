97fd8fada040c0af882ad159df45028a692668b9
sarika-natu <natu.sarika89@gmail.com>
2020-09-28 22:40:58 -0700

    Part 3.1 Extra functionality: Implemented RGB led using HSV coding style

--- projects/lpc40xx_freertos/l5_application/main.c
+++ projects/lpc40xx_freertos/l5_application/main.c
@@ -27,13 +27,34 @@ static QueueHandle_t adc_to_pwm_task_queue;
 #define PWM_FREQ 1
 #define DUTY_CYCLE_50 50
 #define PERCENTILE 100
+#define RGB_MAX 255
+#define ANGLE_MAX 360
+
+#define PART3_1
 
 typedef struct {
   gpio__port_e port;
   uint8_t pin;
 } port_pin_s;
 
-void pin_configure_pwm_channel_as_io_pin() {
+typedef struct {
+  uint8_t red;
+  uint8_t green;
+  uint8_t blue;
+} rgb_value;
+
+const uint8_t HSVlights[61] = {0,   4,   8,   13,  17,  21,  25,  30,  34,  38,  42,  47,  51,  55,  59,  64,
+                               68,  72,  76,  81,  85,  89,  93,  98,  102, 106, 110, 115, 119, 123, 127, 132,
+                               136, 140, 144, 149, 153, 157, 161, 166, 170, 174, 178, 183, 187, 191, 195, 200,
+                               204, 208, 212, 217, 221, 225, 229, 234, 238, 242, 246, 251, 255};
+
+static void pin_configure_pwm_channel_as_io_pin(void);
+void pwm_task(void *p);
+static rgb_value trueHSV(uint8_t angle);
+static void pin_configure_adc_channel_as_io_pin(void);
+void adc_task(void *p);
+
+static void pin_configure_pwm_channel_as_io_pin(void) {
   const uint32_t func_mask = 0x07;
   const uint32_t enable_pwm_func = 0x01;
   LPC_IOCON->P2_0 &= ~func_mask;
@@ -48,6 +69,49 @@ void pin_configure_pwm_channel_as_io_pin() {
   LPC_IOCON->P2_4 &= ~func_mask;
   LPC_IOCON->P2_4 |= enable_pwm_func;
 }
+#ifdef PART3_1
+/*Source: https://www.instructables.com/id/How-to-Make-Proper-Rainbow-and-Random-Colors-With-/*/
+static rgb_value trueHSV(uint8_t angle) {
+  rgb_value value;
+  uint8_t red, green, blue;
+
+  // Calculate the RGB colors.
+  if (angle < 60) {
+    red = 255;
+    green = HSVlights[angle];
+    blue = 0;
+
+  } else if (angle < 120) {
+    red = HSVlights[120 - angle];
+    green = 255;
+    blue = 0;
+
+  } else if (angle < 180) {
+    red = 0;
+    green = 255;
+    blue = HSVlights[angle - 120];
+
+  } else if (angle < 240) {
+    red = 0;
+    green = HSVlights[240 - angle];
+    blue = 255;
+
+  } else if (angle < 300) {
+    red = HSVlights[angle - 240];
+    green = 0;
+    blue = 255;
+
+  } else {
+    red = 255;
+    green = 0;
+    blue = HSVlights[360 - angle];
+  }
+  value.red = red;
+  value.green = green;
+  value.blue = blue;
+  return value;
+}
+#endif
 void pwm_task(void *p) {
   int adc_reading;
   uint32_t MR_Readch1, MR_Readch2, MR_Readch3, MR_Readch4;
@@ -60,20 +124,63 @@ void pwm_task(void *p) {
   pwm1__init_single_edge(PWM_FREQ);
   // We only need to set PWM configuration once, and the HW will drive
   // the GPIO at 1000Hz, and control set its duty cycle to 50%
+#ifdef Part3_0
   pwm1__set_duty_cycle(PWM1__2_0, DUTY_CYCLE_50);
   pwm1__set_duty_cycle(PWM1__2_1, DUTY_CYCLE_50);
   pwm1__set_duty_cycle(PWM1__2_2, DUTY_CYCLE_50);
   pwm1__set_duty_cycle(PWM1__2_4, DUTY_CYCLE_50);
+#endif
 
   // Continue to vary the duty cycle in the loop
 
   while (1) {
     if (xQueueReceive(adc_to_pwm_task_queue, &adc_reading, 100)) {
+
+#ifdef PART3_1
+      /****
+       * PWM1__2_1 - brown wire -Red color
+       * PWM1__2_2 - blue wire - Blue color
+       * PWM1__2_4 - green wire -Green color
+       * Truth table
+       * Red  Green   Blue    Merge col
+       * ON    ON      OFF     YELLOW
+       * OFF   ON      ON      CYAN
+       * ON    OFF     ON      MAGENTA
+       * ON    ON      ON      WHITE
+       */
+
+      pin_configure_pwm_channel_as_io_pin();
+
+      pwm1__set_duty_cycle(PWM1__2_1, DUTY_CYCLE_50);
+      pwm1__set_duty_cycle(PWM1__2_2, DUTY_CYCLE_50);
+      pwm1__set_duty_cycle(PWM1__2_4, DUTY_CYCLE_50);
+
+      uint8_t rgb_angle = 0;
+      rgb_value rgb_color = {0, 0, 0};
+
+      // calculate angle to get respective rgb color value
+      rgb_angle = (adc_reading * ANGLE_MAX) / ADC_MAX_VALUE;
+      rgb_color = trueHSV(rgb_angle);
+
+      // calculate rgb percentage for duty cycle
+      rgb_color.red = (rgb_color.red / RGB_MAX) * PERCENTILE;
+      rgb_color.green = (rgb_color.green / RGB_MAX) * PERCENTILE;
+      rgb_color.blue = (rgb_color.blue / RGB_MAX) * PERCENTILE;
+
+      pwm1__set_duty_cycle(PWM1__2_1, rgb_color.red);
+      pwm1__set_duty_cycle(PWM1__2_4, rgb_color.green);
+      pwm1__set_duty_cycle(PWM1__2_2, rgb_color.blue);
+
+#endif
+
+#ifdef Part3_0
       uint8_t adc_percentage = (u_int8_t)(((double)adc_reading / ADC_MAX_VALUE) * PERCENTILE);
+      fprintf(stderr, "The value adc_percentage is %d\n\n", adc_percentage);
       pwm1__set_duty_cycle(PWM1__2_0, adc_percentage);
       pwm1__set_duty_cycle(PWM1__2_1, adc_percentage);
       pwm1__set_duty_cycle(PWM1__2_2, adc_percentage);
       pwm1__set_duty_cycle(PWM1__2_4, adc_percentage);
+#endif
       MR_Read0 = LPC_PWM1->MR0;
       fprintf(stderr, "The value at MR0 is %ld\n", MR_Read0);
       MR_Readch1 = LPC_PWM1->MR1;
@@ -97,14 +204,18 @@ void pwm_task(void *p) {
   }
 }
 
-void pin_configure_adc_channel_as_io_pin(void) {
+static void pin_configure_adc_channel_as_io_pin(void) {
   const uint32_t func_mask = 0x07;
   const uint32_t enable_adc_func = 0x01;
   const uint32_t enable_analog_mode = (1u << 7);
+  // const uint32_t enable_pull_mode = (0b01 << 3);
+  // const uint32_t enable_pull_mask = (0b11 << 3);
 
   LPC_IOCON->P0_25 &= ~func_mask;
   LPC_IOCON->P0_25 |= enable_adc_func;     // setting FUNC bits of IOCON
   LPC_IOCON->P0_25 &= ~enable_analog_mode; // setting pin to analog mode
+  // LPC_IOCON->P0_25 &= ~enable_pull_mask;
+  // LPC_IOCON->P0_25 |= enable_pull_mode;
 }
 void adc_task(void *p) {
   int adc_reading;
