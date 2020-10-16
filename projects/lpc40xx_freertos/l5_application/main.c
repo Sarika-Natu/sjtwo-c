99544a864d099294ae9eb8f34fd376c292f3c806
sarika-natu <natu.sarika89@gmail.com>
2020-10-15 21:22:55 -0700

    Part: Producer-Consumer tasks and telemetry outputs

--- projects/lpc40xx_freertos/l5_application/main.c
+++ projects/lpc40xx_freertos/l5_application/main.c
@@ -9,14 +9,99 @@
 #include "periodic_scheduler.h"
 #include "sj2_cli.h"
 
+#include "lpc40xx.h"
+#include "queue.h"
+
 static void create_blinky_tasks(void);
 static void create_uart_task(void);
 static void blink_task(void *params);
 static void uart_task(void *params);
 
+static QueueHandle_t switch_queue;
+#define PHighCHigh
+
+typedef enum { switch__off, switch__on } switch_e;
+
+static switch_e get_switch_input_from_switch(void) {
+  const uint32_t port0_pin30 = (1 << 30);
+
+  switch_e switch_value;
+  if (LPC_GPIO0->PIN & port0_pin30) {
+    switch_value = switch__on;
+  } else {
+    switch_value = switch__off;
+  }
+  return switch_value;
+}
+
+// TODO: Create this task at PRIORITY_LOW
+void producer(void *p) {
+  while (1) {
+    // This xQueueSend() will internally switch context to "consumer" task because it is higher priority than this
+    // "producer" task Then, when the consumer task sleeps, we will resume out of xQueueSend()and go over to the next
+    // line
+
+    // TODO: Get some input value from your board
+    const switch_e switch_value = get_switch_input_from_switch();
+
+    // TODO: Print a message before xQueueSend()
+    printf("%s(), line %d, sending message\n", __FUNCTION__, __LINE__);
+    // Note: Use printf() and not fprintf(stderr, ...) because stderr is a polling printf
+    xQueueSend(switch_queue, &switch_value, 0);
+    // TODO: Print a message after xQueueSend()
+    printf("%s(), line %d, sending message\n", __FUNCTION__, __LINE__);
+
+    vTaskDelay(1000);
+  }
+}
+
+// TODO: Create this task at PRIORITY_HIGH
+void consumer(void *p) {
+  switch_e switch_value;
+  while (1) {
+    // TODO: Print a message before xQueueReceive()
+    printf("%s(), line %d, taking message\n", __FUNCTION__, __LINE__);
+    xQueueReceive(switch_queue, &switch_value, portMAX_DELAY);
+    // TODO: Print a message after xQueueReceive()
+    printf("%s(), line %d, taking message, Switch value: %d\n", __FUNCTION__, __LINE__, switch_value);
+  }
+}
+
+static void initialize_gpio(void) {
+  const uint32_t func_mask = 0x07;
+  const uint32_t enable_gpio_func = 0x00;
+  const uint32_t set_as_output = (1 << 30);
+
+  LPC_IOCON->P0_30 &= ~func_mask;
+  LPC_IOCON->P0_30 |= enable_gpio_func;
+  LPC_GPIO0->DIR |= set_as_output;
+}
+
 int main(void) {
   create_blinky_tasks();
   create_uart_task();
+
+  initialize_gpio();
+
+// TODO: Create your tasks
+#ifdef PHighCLow
+  xTaskCreate(producer, "producer", (512U * 4) / sizeof(void *), (void *)NULL, PRIORITY_HIGH, NULL);
+  xTaskCreate(consumer, "consumer", (512U * 4) / sizeof(void *), (void *)NULL, PRIORITY_LOW, NULL);
+#endif
+
+#ifdef PLowCHigh
+  xTaskCreate(producer, "producer", (512U * 4) / sizeof(void *), (void *)NULL, PRIORITY_LOW, NULL);
+  xTaskCreate(consumer, "consumer", (512U * 4) / sizeof(void *), (void *)NULL, PRIORITY_HIGH, NULL);
+#endif
+
+#ifdef PHighCHigh
+  xTaskCreate(producer, "producer", (512U * 4) / sizeof(void *), (void *)NULL, PRIORITY_HIGH, NULL);
+  xTaskCreate(consumer, "consumer", (512U * 4) / sizeof(void *), (void *)NULL, PRIORITY_HIGH, NULL);
+#endif
+
+  // TODO Queue handle is not valid until you create it
+  // Choose depth of item being our enum (1 should be okay for this example)
+  switch_queue = xQueueCreate(2, sizeof(switch_e));
 
   puts("Starting RTOS");
   vTaskStartScheduler(); // This function never returns unless RTOS scheduler runs out of memory and fails
